5a840897f61f5ad28b4a9222e40e88b265f4c0ac
okumi <okumi@foxmail.com>
2025-06-08 16:16:03 +0800

    2025.6.8

--- Luogu/2434/2434.cpp
+++ Luogu/2434/2434.cpp
@@ -2,35 +2,31 @@
 
 using i64 = long long;
 
-constexpr int N = 50007;
+constexpr int N = 5e4 + 7;
 
-int n, ansl, ansr;
+int n, l, r;
 
-struct line {
+struct node {
 	int l, r;
 } a[N];
 
-bool cmp(const line &x, const line &y) { return x.l < y.l; }
+bool cmp(const node &a, const node &b) { return a.l < b.l; }
 
 int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
 
 	std::cin >> n;
-	for (int i = 1; i <= n; i++)
+	for (int i = 1; i <= n; i++) {
 		std::cin >> a[i].l >> a[i].r;
-
-	std::sort(a + 1, a + n + 1, cmp);
-	ansl = a[1].l, ansr = a[1].r;
-	for (int i = 2; i <= n; i++) {
-		if (a[i].l > ansr) {
-			std::cout << ansl << " " << ansr << "\n";
-			ansl = a[i].l;
-			ansr = std::max(ansr, a[i].r);
-		} else {
-			ansr = std::max(ansr, a[i].r);
-		}
 	}
-	std::cout << ansl << " " << ansr << "\n";
+	std::sort(a + 1, a + n + 1, cmp);
+	l = a[1].l, r = a[1].r;
+	for (int i = 2; i <= n; i++) {
+		if (r < a[i].l) { std::cout << l << " " << r << "\n"; l = a[i].l; }
+		l = std::min(l, a[i].l);
+		r = std::max(r, a[i].r);
+	}
+	std::cout << l << " " << r << "\n";
 	return 0;
 }
